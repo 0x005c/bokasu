44d58221191775c17943358eaf0c9b3762cd95ff
0x005c <52650857+0x005c@users.noreply.github.com>
2019-08-08 02:50:30 +0900

    Add option parser

--- source/app.d
+++ source/app.d
@@ -1,6 +1,17 @@
-import std.stdio;
+import std.stdio, std.math, std.getopt;
+import imageformats;
 
-void main()
-{
-	writeln("Edit source/app.d to start your project.");
+// TODO 引数をパースする　がんばってぼかす
+void main(string[] args) {
+  string mode = "blur"; // mosaic or blur or shrink
+  int strength = 4;
+  string inFileName = "";
+  string outFileName = "";
+  auto helpInformation = getopt(
+      args,
+      "mode|m", &mode,
+      "strength|s", &strength, // strength of mosaic (or blur or shrink)
+      "input|i", &inFileName,
+      "output|o", &outFileName
+      );
 }
